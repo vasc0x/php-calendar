7ae8090fe3a344f48c7f909da82e958923aacfc2
Adan Vasconcelos <avasc007@fiu.edu>
2023-10-13 16:16:26 -0400

    remove keys

--- o365/ClientReg.php
+++ o365/ClientReg.php
@@ -1,12 +1,12 @@
 <?php
 class ClientReg {
   // App's client ID. Register the app in Azure AD to get this value.
-  public static $clientId = "c2eeadb3-96ab-4830-af10-6723f9d4c3e8";
+  public static $clientId = "";
 
   // App's client secret. Register the app in Azure AD to get this value.
-  public static $clientSecret = "dEU8Q~-YROcEjX~2M1zbn10FQ.8XfqYTjDRN8b-c";
+  public static $clientSecret = "";
 
   // App's tenant ID. Register the app in Azure AD to get this value.
-  public static $tenantId = "2a144b72-f239-42d4-8c0e-6f0f17c48e33";
+  public static $tenantId = "";
 }
 ?>
